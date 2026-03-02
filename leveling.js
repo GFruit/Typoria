7378c73242e730d78c9cbe354cbeeca29a1da7a8
GFruit <71150343+GFruit@users.noreply.github.com>
2026-03-02 18:43:19 +0100

    changed leveling curve

--- leveling.js
+++ leveling.js
@@ -3,7 +3,7 @@ const MAX_LEVEL = 99;
 let _lastLevel = 1;
 
 function xpForLevel(level) {
-  return Math.floor(4 * Math.pow(level, 1.5));
+  return Math.floor(500 + 4 * Math.pow(level, 2.5));
 }
 
 function getLevelInfo(totalXp) {
